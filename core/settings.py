c5ff24f6a4e29a739856dfd498c4e1c7d4f15fb1
adminhype <ado.designs@outlook.de>
2025-11-28 23:52:54 +0100

    feat: django-cors-headers

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
 
 # Application definition
 
@@ -47,9 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'upload_app',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
