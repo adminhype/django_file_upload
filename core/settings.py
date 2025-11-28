92958e39136f98f1fc30a151e09643b72e39dd33
adminhype <ado.designs@outlook.de>
2025-11-28 19:55:45 +0100

    feat: upload_app with model and media_root

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'upload_app',
 ]
 
 MIDDLEWARE = [
